a510bc21e835227ab766ae33c295e7ca522bb4d8
Vitória <vitoriasilvan321@gmail.com>
2024-07-02 11:39:20 -0300

    aula 03

---
+++ Desafios_Aula3.py
@@ -0,0 +1,29 @@
+#-----1
+num1 = 2
+num2= 3
+soma = num1 +num2
+print(f'A soma do {num1} + {num2} = {soma}\n')
+
+#-------2
+
+valor1 = int(input('Digite um número inteiro: '))
+valor2 = int(input('Digite um número inteiro: '))
+print('\n')
+print(f'A soma dos números: {valor1} + {valor2} = {valor1 + valor2}')
+print(f'A subtração dos números: {valor1} - {valor2} = {valor1 - valor2}')
+print(f'A multiplicação dos números: {valor1} * {valor2} = {valor1 * valor2}')
+print(f'A divisão dos números: {valor1} \ {valor2} = {valor1 /valor2}\n')
+
+
+#-----3
+
+nome= input('Digite o seu nome: ')
+idade = int(input('Digite a sua idade: '))
+
+print(f'O seu nome é {nome}, você tem {idade} anos!\n')
+
+print('Digite dois valores para realizar a soma: ')
+numero1=int(input('Primeiro número: ')) 
+numero2 = int(input('Segundo numero: '))
+
+print(f'A soma do {numero1} + {numero2} = {numero1+numero2}')
---
+++ aula3.py
@@ -0,0 +1,32 @@
+#--------------OPERADORES-------------
+#Soma
+soma = 10+10
+
+print('Usando como concatenação : 10'+ '10')
+print('Usando soma:',10+10)
+print(soma)
+
+num1 = 50
+num2 = 20
+soma = num1 + num2
+
+print(f'Soma do {num1} + {num2} = {soma}')
+ 
+
+#Subtração
+
+subtracao = num1 - num2
+print(f'Subtração dos números: {num1} - {num2} = {subtracao}')
+
+
+#Multiplicação
+
+multiplicacao = num1 * num2
+print(f'Multiplicação dos números: {num1} * {num2} = {multiplicacao}')
+
+
+#Divisão
+
+divisao = num1 / num2
+print(f'Divisão dos números: {num1} / {num2} = {divisao}')
+
