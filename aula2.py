577b9264f92f84a8c52cc6af11527d85b0261a44
Vitória <vitoriasilvan321@gmail.com>
2024-07-02 08:48:17 -0300

    first commit

---
+++ aula2.py
@@ -0,0 +1,56 @@
+'''
+print("Hello World")
+nome = "gomes"
+print(nome)
+
+peso = 80.5
+altura = 1.75
+intrutor = True
+print("")
+
+print(peso)
+print(altura)
+print(intrutor)
+print("")
+
+print(type(peso))
+print(type(altura))
+print(type(intrutor))
+print(intrutor,type(intrutor))
+
+print("")
+valor = 15
+print(valor,type(valor))
+valor = str(valor)
+print(valor,type(valor))
+print("")
+
+
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura: "))
+
+#print('1° Olá meu nome é ' + nome + ' ! tenho ' +str(idade) + idade +'anos ')
+
+#print('2° Olá, meu nome é ',nome, 'tenho ',idade, 'anos')
+print("")
+
+print(f"Olá, meu nome é {nome}, tenho {idade} anos. Meu peso é {peso} Kg e minha altura é {altura:.2f}m" )
+print("")
+print(nome,type(nome))
+print(idade,type(idade))
+print(peso,type(peso))
+print(altura,type(altura))
+'''
+
+
+#------------------------------------REVISÃO--------------
+nome  = 'Karython'
+valor = 15 
+valor = str(valor)
+
+print(valor)
+
+
