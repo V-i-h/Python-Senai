3e5a8f82f63a95ae96907e4ad59fe6b6bc4bfb95
Vitória <vitoriasilvan321@gmail.com>
2024-07-02 08:58:18 -0300

    first commit

---
+++ aula3.py
@@ -0,0 +1,12 @@
+
+#------------------------------------REVISÃO--------------
+
+#Declaração de variável
+nome  = 'Karython'
+
+#Convertendo o tipo 
+valor = 15 
+valor = str(valor)
+
+#Restringir os dados
+idade = int(input("Digite sua idade: "))
